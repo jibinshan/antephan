bc598a51383e4757c45f381d32f9d07b1c9c9ad0
Jibinshan <138592544+jibinshan@users.noreply.github.com>
2025-03-31 18:37:34 +0530

    Update Footer.tsx

--- src/app/(section)/Footer.tsx
+++ src/app/(section)/Footer.tsx
@@ -57,7 +57,7 @@ const Footer: React.FC = () => {
                   <Link href="/">Home</Link>
                 </Button>
               </li>
-              <li>
+{/*               <li>
                 {" "}
                 <Button
                   asChild
@@ -66,7 +66,7 @@ const Footer: React.FC = () => {
                 >
                   <Link href="/menu">Menu</Link>
                 </Button>
-              </li>
+              </li> */}
               <li>
                 <Button
                   asChild
